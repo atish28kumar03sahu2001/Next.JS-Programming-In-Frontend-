c8dab2dbb2a731e537f6df63efa8b0625c57debb
Atish Kumar Sahu <160933920+kumar28sahu03atish01@users.noreply.github.com>
2024-08-10 19:26:57 +0530

    Delete JavaScript 01 directory

--- JavaScript 01/model/Image.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const ImageSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  url: { type: String, required: true },
-});
-
-export default mongoose.models.Image || mongoose.model('Image', ImageSchema);
