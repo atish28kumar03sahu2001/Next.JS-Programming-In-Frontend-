f46a77728ba4440c7bdd158a3843094c90442955
Atish Kumar Sahu <160933920+kumar28sahu03atish01@users.noreply.github.com>
2024-08-16 00:13:15 +0530

    Delete Demo-Project - 01 directory

--- Demo-Project - 01/src/data/getData.js
+++
@@ -1,9 +0,0 @@
-import { fetchAuthUserAction } from "@/actions";
-export async function getData() {
-    const result = await fetchAuthUserAction();
-    if (result.success) {
-        return result.data;
-    } else {
-        throw new Error(result.message);
-    }
-}
--- Demo-Project - 01/src/models/index.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const UserSchema = new mongoose.Schema({
-    userName: String,
-    userEmail: String,
-    userPassword: String,
-})
-export const User = mongoose.models.User || mongoose.model('User',UserSchema);
