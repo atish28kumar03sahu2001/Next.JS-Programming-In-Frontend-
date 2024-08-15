3f6dce601fc42e104eb5ae0ede917faf744c145d
Atish Kumar Sahu <160933920+kumar28sahu03atish01@users.noreply.github.com>
2024-08-16 00:13:30 +0530

    Delete Next Authentication - 01 directory

--- Next Authentication - 01/src/database/index.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-export const connectToDB = async () => {
-    const connectionUrl = "mongodb+srv://kumarsahuatish:TyzfjJK2Ky40AmPj@auth.oziov.mongodb.net/";
-    mongoose
-        .connect(connectionUrl)
-        .then(()=>console.log("Connection Successful!"))
-        .catch(e=>console.log(e))
-};
